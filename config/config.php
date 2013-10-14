9897dfac41856c33799a8fa2e99dce78b070bc03
Pathana Chonmany <admin@pchonmany.com>
2013-10-14 16:20:40 +0700

    Created the sign up, login, and logout pages and modified controllers in order to do this

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'BlogByte');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	define('DB_NAME', "p2_pchonmany_biz"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
@@ -43,5 +43,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	define('PASSWORD_SALT', 'DV!S9v?UKGgT+Tx%MnFjc8$Zdh#a8@k7ycs~XWEr');
+	define('TOKEN_SALT', 'Bn47S~TdPym#tDcx!DDG@CgF?Yah7j2VqvM8QNkH');
--- views/_v_template.php
+++ views/_v_template.php
@@ -3,11 +3,14 @@
 <head>
 	<title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
-	<!-- Controller Specific JS/CSS -->
-	<?php if(isset($client_files_head)) echo $client_files_head; ?>
-	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />   
+    <!-- Common CSS/JSS -->
+    <link rel="stylesheet" href="/css/app.css" type="text/css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <!-- Controller Specific CSS/JS -->
+    <?php if(isset($client_files_head)) echo $client_files_head; ?>
+
 </head>
 
 <body>	
