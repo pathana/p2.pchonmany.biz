618db2ed7bd9fed6249d4eea2b51907ef18d2265
Pathana Chonmany <admin@pchonmany.com>
2013-10-15 17:20:20 +0700

    Updated my email address line

--- config/config.php
+++ config/config.php
@@ -28,7 +28,7 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'pchonmany@g.harvard.edu'); 
 
 # Default DB name for this app
 	define('DB_NAME', "pchonman_p2_pchonmany_biz"); 
