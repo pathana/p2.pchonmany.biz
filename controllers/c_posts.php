7642ad451d34b4bcb1b85e2fc4e5a8ca17c3cda2
Pathana Chonmany <admin@pchonmany.com>
2013-10-27 22:51:47 +0700

    Added the index function

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -42,29 +42,35 @@ class posts_controller extends base_controller {
 
     public function index() {
 
-        # Set up the View
-        $this->template->content = View::instance('v_posts_index');
-        $this->template->title   = "Posts";
+    # Set up the View
+    $this->template->content = View::instance('v_posts_index');
+    $this->template->title   = "All Posts";
 
-        # Build the query
-        $q = "SELECT 
-                posts.*, 
-                users.first_name, 
-                users.last_name 
-            FROM posts
-            INNER JOIN users ON posts.user_id = users.user_id";
+    # Query
+    $q = 'SELECT 
+            posts.content,
+            posts.created,
+            posts.user_id AS post_user_id,
+            users_users.user_id AS follower_id,
+            users.first_name,
+            users.last_name
+        FROM posts
+        INNER JOIN users_users 
+            ON posts.user_id = users_users.user_id_followed
+        INNER JOIN users 
+            ON posts.user_id = users.user_id
+        WHERE users_users.user_id = '.$this->user->user_id;
 
-        # Run the query
-        $posts = DB::instance(DB_NAME)->select_rows($q);
+    # Run the query, store the results in the variable $posts
+    $posts = DB::instance(DB_NAME)->select_rows($q);
 
-        # Pass data to the View
-        $this->template->content->posts = $posts;
+    # Pass data to the View
+    $this->template->content->posts = $posts;
 
-        # Render the View
-        echo $this->template;
+    # Render the View
+    echo $this->template;
 
     }
-    
     public function users() {
 
         # Set up the View
