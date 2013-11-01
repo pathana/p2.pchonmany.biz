181988eca0a58b114109301cdb8bc8df821d42c0
Pathana Chonmany <admin@pchonmany.com>
2013-11-02 00:23:02 +0700

    Added an image to the sign up page and form validation

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,22 +1,24 @@
 <div id="content">
+
     <h1>Sign Up</h1><p>
+    <img src="/images/byte.jpg" class="picByte">
 
         <form method='POST' action='/users/p_signup'>
 
             First Name:<br>
-            <input type='text' name='first_name' size="30" placeholder="John">
+            <input type='text' name='first_name' size="35" placeholder="John" title="Please enter your first name" required>
             <br><br>
 
             Last Name:<br>
-            <input type='text' name='last_name' size="30" placeholder="Smith">
+            <input type='text' name='last_name' size="35" placeholder="Smith" title="Please enter your last name" required>
             <br><br>
 
             Email:<br>
-            <input type='text' name='email' size="30" placeholder="example@email.com">
+            <input type='text' name='email' size="35" placeholder="example@email.com" title="Please enter a valid email address" required>
             <br><br>
 
             Password:<br>
-            <input type='password' name='password' size="30">
+            <input type='password' name='password' size="35">
             <p><br><br>
 
             <button type='submit' class="submit">Sign Up</button>
