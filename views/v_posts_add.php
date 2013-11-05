44f34361b422f9d9ebdb478e7acbc00825b41d32
Pathana Chonmany <admin@pchonmany.com>
2013-11-05 14:01:28 +0700

    Modified the New Post page with CSS for the text area and submit button

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,9 +1,11 @@
-<form method='POST' action='/posts/p_add'>
+<div id="content">
+	<form method='POST' action='/posts/p_add'>
 
-    <label for='content'>New Post:</label><br>
-    <textarea name='content' id='content'></textarea>
+	    <label for='content'><h1>Add a New Post:</h1></label><br>
+	    <textarea name='content' id='content' required maxlength="1000" cols="10" rows="10"></textarea>
 
-    <br><br>
-    <input type='submit' value='New post'>
+	    <br><br>
+	    <button type='submit' class="submit">New Post</button>
 
-</form> 
+	</form>
+</div>
