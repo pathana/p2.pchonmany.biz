4bea7823883cf5fbec10d48ca93754cab058b071
Pathana Chonmany <admin@pchonmany.com>
2013-11-06 13:55:41 +0700

    Modified profile page to show SQL strings from user

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,10 +1,12 @@
-<div id="content">
+<div id="profileContent">
 
     <h1>Profile of <?=$user->first_name?></h1>
 
+        <b>User ID:</b> <?=$user->user_id?><br>
         <b>First Name:</b> <?=$user->first_name?><br>
         <b>Last Name:</b> <?=$user->last_name?><br>
         <b>Email:</b> <?=$user->email?><br>
+
 
 
 
