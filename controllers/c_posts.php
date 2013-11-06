ab887519a760cec0d231c396f124bd1987ac3380
Pathana Chonmany <admin@pchonmany.com>
2013-11-06 13:45:11 +0700

    Created the look for the user profile page

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -60,6 +60,7 @@ class posts_controller extends base_controller {
         INNER JOIN users 
             ON posts.user_id = users.user_id
         WHERE users_users.user_id = '.$this->user->user_id;
+        # ORDER BY posts.created DESC
 
     # Run the query, store the results in the variable $posts
     $posts = DB::instance(DB_NAME)->select_rows($q);
--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,23 +1,12 @@
-<h1>This is the profile of <?=$user->first_name?></h1>
+<div id="content">
 
-<?php
-public function profile($user_name == NULL) {
+    <h1>Profile of <?=$user->first_name?></h1>
 
-    /*
-    If you look at _v_template you'll see it prints a $content variable in the <body>
-    Knowing that, let's pass our v_users_profile.php view fragment to $content so 
-    it's printed in the <body>
-    */
-    $this->template->content = View::instance('v_users_profile');
+        <b>First Name:</b> <?=$user->first_name?><br>
+        <b>Last Name:</b> <?=$user->last_name?><br>
+        <b>Email:</b> <?=$user->email?><br>
 
-    /* $title is another variable used in _v_template to set the <title> of the page
-    $this->template->title = "Profile";
 
-    # Pass information to the view fragment
-    $this->template->content->user_name = $user_name;
 
-    # Render View
-    echo $this-template;
+</div>
 
-}
-?>
